7d75cd270f73499a3344d9d9d12c1f5d3a67682a
alexhungerford <50681223+alexhungerford@users.noreply.github.com>
2019-05-21 13:48:48 -0700

    changed on github.com for fetch practice

--- script.R
+++ script.R
@@ -7,3 +7,5 @@ x <- 10
 # Updating to test fetch/pull/branching.
 # New change/add/commit after I created a pull request but before it was merged
 # by original repo owner. Both commits show in the open pull request.
+
+# New change done on github.com. For purpose of fetching.
